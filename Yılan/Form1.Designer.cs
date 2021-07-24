41e2b15b3f2e323270144d104d047b3fd3c1a70b
Mustafa Yılmaz Çakıcı <55806456+wotanfinrod@users.noreply.github.com>
2021-07-24 13:31:06 +0300

    Add files

---
+++ Yılan/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Yılan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.labelyem = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.timersol = new System.Windows.Forms.Timer(this.components);
+            this.timersağ = new System.Windows.Forms.Timer(this.components);
+            this.timeryukarı = new System.Windows.Forms.Timer(this.components);
+            this.timeraşağı = new System.Windows.Forms.Timer(this.components);
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(230, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 12);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(243, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 12);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(255, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 12);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(268, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 12);
+            this.label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(281, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 12);
+            this.label5.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(287, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 12);
+            this.label6.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(300, 160);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 12);
+            this.label7.TabIndex = 6;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(306, 160);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 12);
+            this.label8.TabIndex = 7;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(319, 160);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 12);
+            this.label9.TabIndex = 8;
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(325, 160);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(13, 12);
+            this.label10.TabIndex = 9;
+            // 
+            // labelyem
+            // 
+            this.labelyem.BackColor = System.Drawing.Color.Black;
+            this.labelyem.Location = new System.Drawing.Point(220, 160);
+            this.labelyem.Name = "labelyem";
+            this.labelyem.Size = new System.Drawing.Size(13, 12);
+            this.labelyem.TabIndex = 10;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.Location = new System.Drawing.Point(578, 390);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(34, 25);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "00";
+            // 
+            // timersol
+            // 
+            this.timersol.Tick += new System.EventHandler(this.timersol_Tick);
+            // 
+            // timersağ
+            // 
+            this.timersağ.Tick += new System.EventHandler(this.timersağ_Tick);
+            // 
+            // timeryukarı
+            // 
+            this.timeryukarı.Tick += new System.EventHandler(this.timeryukarı_Tick);
+            // 
+            // timeraşağı
+            // 
+            this.timeraşağı.Tick += new System.EventHandler(this.timeraşağı_Tick);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(588, 440);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(84, 13);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "M. Yılmaz Çakıcı";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label13.Location = new System.Drawing.Point(551, 9);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(0, 17);
+            this.label13.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGreen;
+            this.ClientSize = new System.Drawing.Size(684, 462);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.labelyem);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Yılan Reborn";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label labelyem;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Timer timersol;
+        private System.Windows.Forms.Timer timersağ;
+        private System.Windows.Forms.Timer timeryukarı;
+        private System.Windows.Forms.Timer timeraşağı;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ Yılan/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Yılan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        int puan = 0;
+        SoundPlayer tık = new SoundPlayer(Properties.Resources.Kick___Sound_Effect);
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+
+            if (e.KeyCode == Keys.Left && timersağ.Enabled != true)
+            {
+                timersol.Enabled = true;
+                timersağ.Enabled = false;
+                timeraşağı.Enabled = false;
+                timeryukarı.Enabled = false;
+            }
+            if (e.KeyCode == Keys.Right && timersol.Enabled != true)
+            {
+                timersol.Enabled = false;
+                timersağ.Enabled = true;
+                timeraşağı.Enabled = false;
+                timeryukarı.Enabled = false;
+            }
+            if (e.KeyCode == Keys.Up && timeraşağı.Enabled !=true)
+            {
+
+                timersol.Enabled = false;
+                timersağ.Enabled = false;
+                timeraşağı.Enabled = false;
+                timeryukarı.Enabled = true;
+            }
+            if (e.KeyCode == Keys.Down && timeryukarı.Enabled !=true)
+            {
+                timersol.Enabled = false;
+                timersağ.Enabled = false;
+                timeraşağı.Enabled = true;
+                timeryukarı.Enabled = false;
+            }
+
+          
+
+           
+            
+        }
+
+        void kontrolyem()
+        {
+            int yemx = rnd.Next(5, 61);
+            int yemy = rnd.Next(5, 41);
+
+            if (label1.Location.X == labelyem.Location.X && label1.Location.Y == labelyem.Location.Y)
+            {
+                tık.Play();
+                labelyem.Location = new Point(yemx * 10, yemy * 10);
+
+                if (puan >= 90) puan++;
+                if (puan < 90) puan += 10;
+               
+         
+                label11.Text = puan.ToString(); 
+
+            } 
+        }
+        void kontrolkaybetme()
+        {
+            
+            if (label1.Left <= 0 || label1.Right >= 700 || label1.Top <= 0 || label1.Bottom >= 500 || label1.Location == label3.Location || label1.Location == label4.Location || label1.Location == label5.Location || label1.Location == label6.Location || label1.Location == label6.Location || label1.Location ==label7.Location || label1.Location == label8.Location || label1.Location == label9.Location || label1.Location == label10.Location)
+            {
+                DialogResult baslama = new DialogResult();
+                timersağ.Enabled = false;
+                timersol.Enabled = false;
+                timeraşağı.Enabled = false;
+                timeryukarı.Enabled = false;
+
+                MessageBox.Show("Kaybettiniz. Puanınız "+puan+".");
+               
+                baslama = MessageBox.Show("Yeniden Oynamak İstiyor Musunuz?", "Kaybettiniz", MessageBoxButtons.YesNo);
+                if (baslama == DialogResult.Yes) yenidenbasla();
+                else if (baslama == DialogResult.No) Application.Exit();             
+            }
+
+            if (puan == 99)
+            {
+                timersağ.Enabled = false;
+                timersol.Enabled = false;
+                timeraşağı.Enabled = false;
+                timeryukarı.Enabled = false;
+
+                MessageBox.Show("Tebrikler, kazandiniz!");
+                Application.Exit();
+                
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timersol.Enabled = false;
+        }
+
+        private void timersol_Tick(object sender, EventArgs e)
+        {
+            label1.Location = new Point(label1.Left - 10, label1.Top);
+            kontrolyem();
+            sürükleme();           
+            kontrolkaybetme();
+            hızlanma();
+        }
+
+        private void timersağ_Tick(object sender, EventArgs e)
+        {
+            label1.Location = new Point(label1.Left + 10, label1.Top);
+            sürükleme();
+            kontrolyem();
+            kontrolkaybetme();
+            hızlanma();
+
+        }
+
+        private void timeryukarı_Tick(object sender, EventArgs e)
+        {
+            label1.Location = new Point(label1.Left, label1.Top - 10);
+            sürükleme();
+            kontrolyem();
+            kontrolkaybetme();
+            hızlanma();
+        }
+
+        private void timeraşağı_Tick(object sender, EventArgs e)
+        {
+            label1.Location = new Point(label1.Left, label1.Top + 10);
+            sürükleme();
+            kontrolyem();
+            kontrolkaybetme();
+        }
+
+        void sürükleme()
+        {
+            label10.Location = new Point(label9.Location.X, label9.Location.Y);
+            label9.Location = new Point(label8.Location.X, label8.Location.Y);
+            label8.Location = new Point(label7.Location.X, label7.Location.Y);
+            label7.Location = new Point(label6.Location.X, label6.Location.Y);
+            label6.Location = new Point(label5.Location.X, label5.Location.Y);
+            label5.Location = new Point(label4.Location.X, label4.Location.Y);
+            label4.Location = new Point(label3.Location.X, label3.Location.Y);
+            label3.Location = new Point(label2.Location.X, label2.Location.Y);
+            label2.Location = new Point(label1.Location.X, label1.Location.Y);
+        }
+        void yenidenbasla()
+        {
+            puan = 0;
+            label11.Text = puan.ToString();
+            labelyem.Location = new Point(220, 160);
+            label1.Location = new Point(230,160);
+            label2.Location = new Point(240,160);
+            label3.Location = new Point(250,160);
+            label4.Location = new Point(260,160);
+            label5.Location = new Point(270,160);
+            label6.Location = new Point(280,160);
+            label7.Location = new Point(290,160);
+            label8.Location = new Point(300,160);
+            label9.Location = new Point(310,160);
+            label10.Location = new Point(320,160);
+        }
+
+        void hızlanma()
+        {
+            
+                timersağ.Interval =100- puan ;
+                timersol.Interval =100- puan ;
+                timeraşağı.Interval =100- puan ;
+                timeryukarı.Interval =100- puan ;
+            
+        }
+
+        
+
+        
+       
+    }
+}
